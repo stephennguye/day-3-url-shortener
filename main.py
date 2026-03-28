6dc183955e785cee473f516612279465e346d9c6
Nhat Anh NT <stephen.ca.work@gmail.com>
2026-03-28 11:54:13 +0700

    Create shortener function

---
+++ main.py
@@ -0,0 +1,3 @@
+from shortener import URLShortener
+
+
--- shortener.py
+++ shortener.py
@@ -1,2 +1,12 @@
 import hashlib
 
+class URLShortener:
+    def __init__(self):
+        self.url_mapping = {}
+        self.base_url = "https://short.url/"
+
+    def shorten_url(self, original_url):
+        hashed_url = hashlib.md5(original_url.encode()).hexdigest()[:7]
+        short_url = self.base_url + hashed_url
+        self.url_mapping[short_url] = original_url
+        return short_url
