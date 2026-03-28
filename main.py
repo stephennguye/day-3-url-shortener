84deb9830ce478d64d0d09450c83e4bd455c2afe
Nhat Anh NT <stephen.ca.work@gmail.com>
2026-03-28 12:23:28 +0700

    Create flask app

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from shortener import URLShortener
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+shortener = URLShortener()
 
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    try: 
+        if request.method == 'POST':
+            original_url = request.form["url"]
+            short_url = shortener.shorten_url(original_url)
+            return render_template("ui.html", new_url=short_url, old_url=original_url)
+        else:
+            return render_template("ui.html")
+    
+    except Exception as e:
+        return f"Error: {e}"
+    
+if __name__ == "__main__":
+    app.run()
+
+    
